2250b614192602ad21231827ba8e7db347689e4f
Iagokoch <iagon.koch@gmail.com>
2025-03-04 17:23:31 -0300

    começando a ver ternario

---
+++ melhorias/ternario.rb
@@ -0,0 +1,5 @@
+nome = "Iago"
+
+#condicao ? valor_se_verdadeiro : valor_se_falso
+
+nome.eql?("Iago") ? "futuro garoto de programa" : "não é o Iago" 
---
+++ metodos/calculo.rb
@@ -0,0 +1,9 @@
+def soma (x, y ,z)
+
+ x + y + z
+
+end
+
+resultado = soma(5, 3, 6)
+
+puts resultado
